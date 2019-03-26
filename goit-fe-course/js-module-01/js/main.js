1ca129f01e1393ccdb94a3bfeaf037263beedbf3
Matvey Sokolov <bezyn4uk@hotmail.com>
2019-03-26 12:11:47 +0200

    JS course - lesson 1 submit 1

--- goit-fe-course/js-module-01/js/main.js
+++ goit-fe-course/js-module-01/js/main.js
@@ -46,56 +46,53 @@ for (const quantity of quantities) {
 }
 
 /*
-
  * Задача 3
-
-Пользователь может оформить доставку товара к себе в страну, указав ее в переменной country, но доставка есть не везде.
-
-Напиши скрипт который выводит сообщение о стоимости доставки в указанную страну.
-
-Формат сообщения - Доставка в[страна] будет стоить[цена]
-
-Ниже приведен список стран и стоимость доставки.
-
-Китай - 100 кредитов
-Южная Америка - 250 кредитов
-Австралия - 170 кредитов
-Индия - 80 кредитов
-Ямайка - 120 кредитов
-
-Если указанной страны нет в списке, то выводи в консоль сообщение 'В вашей стране доставка не доступна'.Протестируй работоспособность кода подставив разные значения в переменную country.
-
-    PS: используй switch
 */
 
 function getTravelCost(country) {
+
     let cost;
+    let haveMatched = false;
+
+    if(!country || typeof country != "string") {
+        console.log('В качестве аргумента передана не строка.');
+        return;
+    }
 
     switch (country.toLowerCase()) {
         case 'китай':
             cost = 100;
+            haveMatched = true;
             break;
         case 'южная америка':
             cost = 250;
+            haveMatched = true;
             break;
         case 'австралия':
             cost = 170;
+            haveMatched = true;
             break;
         case 'индия':
             cost = 80;
+            haveMatched = true;
             break;
         case 'ямайка':
             cost = 120;
+            haveMatched = true;
             break;
         default:
-            console.log('В вашей стране доставка не доступна');
             break;
     }
 
-    console.log(`Доставка в ${country} будет стоить ${cost}`);
+
+    if(haveMatched) {
+        console.log(`Доставка в ${country} будет стоить ${cost}`);
+    } else {
+        console.log('В вашей стране доставка не доступна');
+    }
 }
 
-const countries = ['Китай', 'Южная Америка', 'Австралия', 'Индия', 'Ямайка'];
+const countries = ['Китай', 'Южная Америка', 'Австралия', 'Индия', 'Ямайка', 'молоко', null, true, 0, 1];
 
 for (const country of countries) {
     getTravelCost(country);
