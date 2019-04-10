0e2c6c020bc8e7fc5baddfa0cafe9ccf3b80392e
bezyn4uk <bezyn4uk@gmail.com>
2019-04-10 19:16:01 +0300

    git_ignore change

---
+++ goit-fe-course/js-homeworks/tasks_01.js
@@ -0,0 +1,14 @@
+/* jshint esversion: 6 */
+/* jshint strict: global */
+/* jshint devel: true */
+
+'use strict';
+
+
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
+
+console.log(found);
+// expected output: Array ["T", "I"]
