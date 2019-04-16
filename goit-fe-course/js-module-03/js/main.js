e649d5a862a8fd23b12eaf90dc54e2c424a4654a
bezyn4uk <bezyn4uk@gmail.com>
2019-04-16 14:59:30 +0300

    Excercise 3. 3 change validation function.

--- goit-fe-course/js-module-03/js/main.js
+++ goit-fe-course/js-module-03/js/main.js
@@ -10,34 +10,32 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
 const isLoginValid = login => {
-    // return login.length >= 4 && login.length <= 16;
-    return /^[a-z]{4,16}$/gi.test(login);
+    const trimmedLoginLength = login.trim().length;
+    return trimmedLoginLength >= 4 && trimmedLoginLength <= 16;
 };
 
 const isLoginUnique = function (allLogins, login) {
     return !allLogins.includes(login);
 };
 
-const isStrEmpty = function (login) {
-    return !login || /\s/.test(login);
-};
-
 const addLogin = function (allLogins, login) {
-    if (isStrEmpty(login)) {
-        console.log('Строка пустая.');
-        return;
-    } else if(!isLoginValid(login)) {
+
+    if(!isLoginValid(login)) {
         console.log('Ошибка! Логин должен быть от 4 до 16 символов');
-    } else if (!isLoginUnique(allLogins, login)) {
-        console.log('Такой логин уже используется!');
-    } else {
-        allLogins.push(login);
-        console.log('Логин успешно добавлен!');
+        return;
     }
+
+    if (!isLoginUnique(allLogins, login)) {
+        console.log('Такой логин уже используется!');
+        return;
+    }
+
+    allLogins.push(login);
+    console.log('Логин успешно добавлен!');
+
 };
 
 // Вызовы функции для проверки
-
 addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
 addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
 addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
@@ -45,11 +43,7 @@ addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин долж
 addLogin(logins, 'jqueryisext');
 addLogin(logins, 'jqueryisext');
 addLogin([], 'jqueryisex');
-addLogin([], ' ');
-addLogin([], null);
-addLogin([], 0);
-addLogin([], 1);
-addLogin([], undefined);
+addLogin([], '        ');
 
 
 
--- goit-fe-course/js-module-04/js/main.js
+++ goit-fe-course/js-module-04/js/main.js
@@ -6,53 +6,9 @@
 
 'use strict';
 
-
-const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
-
-const isLoginValid = login => {
-    return login.length >= 4 && login.length <= 16;
-};
-
-const isLoginUnique = function (allLogins, login) {
-    return !allLogins.includes(login);
-};
-
-const isStrEmpty = function (login) {
-    return !login || /\s/.test(login);
-};
-
-const addLogin = function (allLogins, login) {
-    if (isStrEmpty(login)) {
-        console.log('Строка пустая.');
-        return;
-    }
-
-    if (isLoginValid(login) && isLoginUnique(allLogins, login)) {
-        allLogins.push(login);
-        console.log('Логин успешно добавлен!');
-    }
-    else if (!isLoginUnique(allLogins, login)) {
-        console.log('Такой логин уже используется!');
-    }
-    else {
-        console.log('Ошибка! Логин должен быть от 4 до 16 символов');
-    }
-
-};
-
-// Вызовы функции для проверки
-
-addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
-addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
-addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-addLogin(logins, 'jqueryisext');
-addLogin(logins, 'jqueryisext');
-addLogin([], 'jqueryisex');
-addLogin([], ' ');
-addLogin([], null);
-addLogin([], 0);
-addLogin([], undefined);
-
-
-
+const notepad = {
+    id: string | int,
+    title: string,
+    body: string,
+    priority: int
+}
