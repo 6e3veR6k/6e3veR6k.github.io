89bb0a57d63374028c384f0ecebfaf712887ee45
bezyn4uk <bezyn4uk@gmail.com>
2019-05-15 20:03:14 +0300

    Third task ( Closes #3 )

--- goit-fe-course/js-module-07/js/main.js
+++ goit-fe-course/js-module-07/js/main.js
@@ -14,3 +14,17 @@ const getUsersByEyeColor = (users, color) => users.filter(user => user.eyeColor 
 
 console.log(getUsersByEyeColor(users, 'blue'));
 // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+// const getUsersByGender = (users, gender) => {
+//     return users.reduce((acc, user) => {
+//         if (user.gender == gender) {
+//             acc.push(user.name);
+//         }
+//         return acc;
+//     }, []);
+// };
+
+const getUsersByGender = (users, gender) => users.filter(user => user.gender == gender).map(user => user.name);
+
+console.log(getUsersByGender(users, 'male'));
+// [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
