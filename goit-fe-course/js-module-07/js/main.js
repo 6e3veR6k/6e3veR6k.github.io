ee66c8807f5f6a226b8d4cf83dd0822c6d2d34de
bezyn4uk <bezyn4uk@gmail.com>
2019-05-15 20:16:18 +0300

    Sixth task ( Closes #6 )

--- goit-fe-course/js-module-07/js/main.js
+++ goit-fe-course/js-module-07/js/main.js
@@ -43,3 +43,11 @@ console.log(getUserByEmail(users, 'shereeanthony@kog.com'));
 // {объект пользователя Sheree Anthony}
 console.log(getUserByEmail(users, 'elmahead@omatom.com'));
 // {объект пользователя Elma Head}
+
+
+const getUsersWithAge = (users, min, max) => users.filter(user => user.age > min && user.age < max);
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
