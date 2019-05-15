cca948119df4e125f969299a19c69858369cd010
bezyn4uk <bezyn4uk@gmail.com>
2019-05-15 19:39:36 +0300

    First task ( Closes #1 )

--- goit-fe-course/js-module-07/js/main.js
+++ goit-fe-course/js-module-07/js/main.js
@@ -3,3 +3,9 @@
 /* jshint devel: true */
 
 'use strict';
+
+
+const getAllNames = users => users.map(user => user.name);
+
+console.log(getAllNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
