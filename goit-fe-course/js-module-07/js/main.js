3a471c0dcdbe0cb136eb93444daacc649301b8c5
bezyn4uk <bezyn4uk@gmail.com>
2019-05-15 19:47:00 +0300

    Second task ( Closes #2 )

--- goit-fe-course/js-module-07/js/main.js
+++ goit-fe-course/js-module-07/js/main.js
@@ -9,3 +9,8 @@ const getAllNames = users => users.map(user => user.name);
 
 console.log(getAllNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+const getUsersByEyeColor = (users, color) => users.filter(user => user.eyeColor == color);
+
+console.log(getUsersByEyeColor(users, 'blue'));
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
