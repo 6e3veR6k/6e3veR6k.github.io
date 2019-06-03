d8cbd31aa99b4c597362fa09eac1631232892b70
Matvey Sokolov <bezyn4uk@hotmail.com>
2019-06-03 16:27:29 +0300

    Homework 9 finished

--- goit-fe-course/js-module-09/js/main.js
+++ goit-fe-course/js-module-09/js/main.js
@@ -6,6 +6,8 @@
 import galleryItems from './gallery-items.js';
 
 const gallery = document.querySelector('.js-gallery');
+const overlay = document.querySelector('.overlay');
+const btnClose = overlay.querySelector('button[data-action="close-modal"]');
 
 const createListItem = function createListItem({ preview, original, description }) {
     const li = document.createElement('li');
@@ -39,3 +41,49 @@ const createList = function createList(listOfImg) {
 
 
 createList(galleryItems);
+
+gallery.addEventListener('click', onImgClickHandle);
+
+function onImgClickHandle(event) {
+    event.preventDefault();
+
+    const listItemImg = event.target;
+    const targetImgSrc = listItemImg.dataset.source;
+    const targetImgAlt = listItemImg.alt;
+
+    showImg(targetImgSrc, targetImgAlt);
+
+    btnClose.addEventListener('click', onClickCloseIconHandle);
+    window.addEventListener('keydown', onEscKeyDownHandle);
+}
+
+function showImg(targetImgSrc, targetImgAlt) {
+    const imgContainer = overlay.querySelector('.content');
+    let img = imgContainer.querySelector('img');
+    // if we change only source in img tag we have few milliseconds when we see old img before new loaded.
+    if (imgContainer.contains(img)) {
+        imgContainer.removeChild(img);
+    }
+    img = `<img src="${targetImgSrc}" alt="${targetImgAlt}" />`;
+    imgContainer.insertAdjacentHTML('beforeend', img);
+    overlay.classList.add('is-visible');
+}
+
+function onClickCloseIconHandle(event) {
+    event.preventDefault();
+    closeOverlay();
+}
+
+function onEscKeyDownHandle(event) {
+    if (event.code == 'Escape') {
+        closeOverlay();
+    }
+    console.log(event.code);
+}
+
+function closeOverlay() {
+    overlay.classList.remove('is-visible');
+    btnClose.removeEventListener('click', onClickCloseIconHandle);
+    overlay.removeEventListener('keydown', onClickCloseIconHandle);
+}
+
