bf46ff5f220f586e58f33560c03852c937e3c68c
Matvey Sokolov <bezyn4uk@hotmail.com>
2020-02-14 16:06:41 +0200

    add empty project

---
+++ goit-fe-course/js-module-homework/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const user = {
+  name: 'Ivan',
+  age: 10
+}
---
+++ recap/module-01/scripts/main-app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("all works");
+
+const userName = "Dmitriy Bezvershuk";
+const len = userName.length;
+
+const userNameUpper = userName.toUpperCase();
+
+let someVar;
+console.log(someVar);
